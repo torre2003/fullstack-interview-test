48ecbe754c123d68e8ad99d7e694c58f91845cbf
Jorge Campos <jcamposlatorre@gmail.com>
2021-07-26 18:54:43 -0400

    add url endpoints

--- flat_project/git_api/urls.py
+++ flat_project/git_api/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 from git_api.apps import GitApiConfig
-
+from .views import (
+    BranchDetailViewSet,
+    BranchListViewSet
+)
 
 app_name = GitApiConfig.name
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        'branches/',
+        BranchListViewSet.as_view({'get': 'list'}),
+        name='branch_list'
+    ),
+    path(
+        'branches/<str:pk>/',
+        BranchDetailViewSet.as_view({'get': 'retrieve'}),
+        name='branch_detail'
+     ),
+]
